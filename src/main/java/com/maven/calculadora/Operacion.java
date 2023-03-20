d69a24ed2b80a639aef8e6691464b6e1ac94f8f7
David González <dp@phoenixconnection.es>
2023-03-20 09:49:48 +0100

    Cración de la clase Operación.

---
+++ src/main/java/com/maven/calculadora/Operacion.java
@@ -0,0 +1,40 @@
+package com.maven.calculadora;
+
+public class Operacion {
+    private double operando1;
+    private double operando2;
+    private double resultado;
+
+    public Operacion() {
+    }
+
+    public Operacion(double operando1, double operando2, double resultado) {
+        this.operando1 = operando1;
+        this.operando2 = operando2;
+        this.resultado = resultado;
+    }
+
+    public double getOperando1() {
+        return operando1;
+    }
+
+    public void setOperando1(double operando1) {
+        this.operando1 = operando1;
+    }
+
+    public double getOperando2() {
+        return operando2;
+    }
+
+    public void setOperando2(double operando2) {
+        this.operando2 = operando2;
+    }
+
+    public double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(double resultado) {
+        this.resultado = resultado;
+    }
+}
